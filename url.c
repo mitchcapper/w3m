ed32ca10022b796eb198730496d3fdfbb98f6b45
Rene Kita <mail@rkta.de>
2024-02-12 16:08:00 +0100

    Fix gopher selector string
    As can be seen in the gopher RFC[0] in the examples on pages 4 and 5 the
    client is supposed to terminate the selection string with CR LF.
    
    I only could track this bug back to the initial revision in the CVS
    history, I would guess that it was present since the beginning of gopher
    support. So, it seems it's not such a big deal, but I heard people
    complain about it - just let's stick to the standard.
    
    [0]: https://www.rfc-editor.org/rfc/rfc1436.html#section-2

--- url.c
+++ url.c
@@ -1954,7 +1954,7 @@ openURL(char *url, ParsedURL *pu, ParsedURL *current,
 	    if (pu->file == NULL)
 		pu->file = "1";
 	    tmp = Strnew_charp(file_unquote(pu->file));
-	    Strcat_char(tmp, '\n');
+	    Strcat_charp(tmp, "\r\n");
 	}
 	write(sock, tmp->ptr, tmp->length);
 	if(type != '\0') {
