3601d308b98006da4240f1fd39282a9245e70264
Rene Kita <mail@rkta.de>
2024-09-16 12:00:45 +0200

    Revert "Fix segfault with dump_source on compressed data"
    This reverts commit e933cf831f32fd4cf763c64aa60e51dbf025ec94.
    
    This commit leads to segmentation faults when using -dump_source on
    compressed data. It depends on another commit which is not committed
    yet.
    
    Also I can't reproduce the issue in any case and I currently don't see
    what got 'fixed' here.  My guess is that I hit a case where bug in
    loadBuffer was triggered and this worked around it by not calling it.

--- file.c
+++ file.c
@@ -2187,13 +2187,6 @@ loadGeneralFile(char *path, ParsedURL *volatile current, char *referer,
 	&& !(w3m_dump & DUMP_EXTRA)) {
 	uncompress_stream(&f, &pu.real_file);
     }
-    /*
-     * TODO(rkta): Something wrong here with DUMP_SOURCE and compressed streams
-     * If DUMP_SOURCE is set and the data is compressed, t will be set to the
-     * type of the compressed data which is not handled in the later if-else
-     * block to determine the function to assign to loadproc. loadproc will be
-     * NULL leading to an error when loading the page.
-    */
     else if (f.compression != CMP_NOCOMPRESS) {
 	if (!(w3m_dump & DUMP_SOURCE) &&
 	    (w3m_dump & ~DUMP_FRAME || is_text_type(t)
@@ -2263,8 +2256,6 @@ loadGeneralFile(char *path, ParsedURL *volatile current, char *referer,
     }
     else if (w3m_dump & DUMP_FRAME)
 	return NULL;
-    else
-	proc = NULL;
 
     if (t_buf == NULL)
 	t_buf = newBuffer(INIT_BUFFER_WIDTH);
