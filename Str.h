408dcb84a227de3a2df335e3af63fb1d27034846
Rene Kita <mail@rkta.de>
2024-09-17 15:32:20 +0200

    Replace STR_SIZE_MAX with STR_LEN_MAX
    We are usually dealing with string length and not with the size a.k.a.
    the length plus the terminating nul character or the allocated
    area_size. The area_size is an implementation detail and should be as
    little visible to the user as possible.
    
    Use a STR_LEN_MAX to avoid dealing with off-by-one errors.

--- Str.h
+++ Str.h
@@ -66,8 +66,8 @@ Str Strfgetall(FILE *);
 
 void Strgrow(Str s);
 
-#define STR_SIZE_MAX (INT_MAX / 32)
-#define Strcat_char(x,y) (((x)->length+1>=STR_SIZE_MAX)?0:(((x)->length+1>=(x)->area_size)?Strgrow(x),0:0,(x)->ptr[(x)->length++]=(y),(x)->ptr[(x)->length]=0))
+#define STR_LEN_MAX (INT_MAX / 32 - 1)
+#define Strcat_char(x,y) (((x)->length>=STR_LEN_MAX)?0:(((x)->length+1>=(x)->area_size)?Strgrow(x),0:0,(x)->ptr[(x)->length++]=(y),(x)->ptr[(x)->length]=0))
 #define Strcmp(x,y)                  strcmp((x)->ptr,(y)->ptr)
 #define Strcmp_charp(x,y)            strcmp((x)->ptr,(y))
 #define Strcasecmp(x,y)              strcasecmp((x)->ptr,(y)->ptr)
