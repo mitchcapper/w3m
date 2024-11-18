b7d29f7bee41c09b226ae30bae330f80bc47748d
Rene Kita <mail@rkta.de>
2024-11-18 07:21:20 +0100

    Refactor fusage()
    See previous commit for a reason.

--- main.c
+++ main.c
@@ -200,83 +200,77 @@ fversion(FILE * f)
 	);
 }
 
+#define PUT(a,b) fprintf(f, "    %-16s %s\n", a, b)
 static void
 fusage(FILE * f, int err)
 {
     fversion(f);
     /* FIXME: gettextize? */
     fprintf(f, "usage: w3m [options] [URL or filename]\noptions:\n");
-    fprintf(f, "    -t tab           set tab width\n");
-    fprintf(f, "    -r               ignore backspace effect\n");
-    fprintf(f, "    -l line          # of preserved line (default 10000)\n");
+    PUT("-t tab", "set tab width");
+    PUT("-r", "ignore backspace effect");
+    PUT("-l line", "# of preserved line (default 10000)");
 #ifdef USE_M17N
-    fprintf(f, "    -I charset       document charset\n");
-    fprintf(f, "    -O charset       display/output charset\n");
+    PUT("-I charset", "document charset");
+    PUT("-O charset", "display/output charset");
 #endif
-    fprintf(f, "    -B               load bookmark\n");
-    fprintf(f, "    -bookmark file   specify bookmark file\n");
-    fprintf(f, "    -T type          specify content-type\n");
-    fprintf(f, "    -m               internet message mode\n");
-    fprintf(f, "    -v               visual startup mode\n");
+    PUT("-B", "load bookmark");
+    PUT("-bookmark file", "specify bookmark file");
+    PUT("-T type", "specify content-type");
+    PUT("-m", "internet message mode");
+    PUT("-v", "visual startup mode");
 #ifdef USE_COLOR
-    fprintf(f, "    -M               monochrome display\n");
-    fprintf(f, "    -H Deprecated! Do not use! Use -o high-intensity=true instead\n");
+    PUT("-M", "monochrome display");
+    PUT("-H Deprecated!", "Do not use! Use -o high-intensity=true instead");
 #endif				/* USE_COLOR */
-    fprintf(f,
-	    "    -N               open URL of command line on each new tab\n");
-    fprintf(f, "    -F               automatically render frames\n");
-    fprintf(f,
-	    "    -cols width      specify column width (used with -dump)\n");
-    fprintf(f,
-	    "    -ppc count       specify the number of pixels per character (4.0...32.0)\n");
+    PUT("-N", "open URL of command line on each new tab");
+    PUT("-F", "automatically render frames");
+    PUT("-cols width", "specify column width (used with -dump)");
+    PUT("-ppc count", "specify the number of pixels per character (4.0...32.0)");
 #ifdef USE_IMAGE
-    fprintf(f,
-	    "    -ppl count       specify the number of pixels per line (4.0...64.0)\n");
+    PUT("-ppl count", "specify the number of pixels per line (4.0...64.0)");
 #endif
-    fprintf(f, "    -dump            dump formatted page into stdout\n");
-    fprintf(f,
-	    "    -dump_head       dump response of HEAD request into stdout\n");
-    fprintf(f, "    -dump_source     dump page source into stdout\n");
-    fprintf(f, "    -dump_both       dump HEAD and source into stdout\n");
-    fprintf(f,
-	    "    -dump_extra      dump HEAD, source, and extra information into stdout\n");
-    fprintf(f, "    -post file       use POST method with file content\n");
-    fprintf(f, "    -header string   insert string as a header\n");
-    fprintf(f, "    +<num>           goto <num> line\n");
-    fprintf(f, "    -num             show line number\n");
-    fprintf(f, "    -no-proxy        don't use proxy\n");
+    PUT("-dump", "dump formatted page into stdout");
+    PUT("-dump_head", "dump response of HEAD request into stdout");
+    PUT("-dump_source", "dump page source into stdout");
+    PUT("-dump_both", "dump HEAD and source into stdout");
+    PUT("-dump_extra", "dump HEAD, source, and extra information into stdout");
+    PUT("-post file", "use POST method with file content");
+    PUT("-header string", "insert string as a header");
+    PUT("+<num>", "goto <num> line");
+    PUT("-num", "show line number");
+    PUT("-no-proxy", "don't use proxy");
 #ifdef INET6
-    fprintf(f, "    -4               IPv4 only (-o dns_order=4)\n");
-    fprintf(f, "    -6               IPv6 only (-o dns_order=6)\n");
+    PUT("-4", "IPv4 only (-o dns_order=4)");
+    PUT("-6", "IPv6 only (-o dns_order=6)");
 #endif
 #ifdef USE_SSL
-    fprintf(f, "    -insecure        use insecure SSL config options\n");
+    PUT("-insecure", "use insecure SSL config options");
 #endif
 #ifdef USE_MOUSE
-    fprintf(f, "    -no-mouse        don't use mouse\n");
+    PUT("-no-mouse", "don't use mouse");
 #endif				/* USE_MOUSE */
 #ifdef USE_COOKIE
-    fprintf(f,
-	    "    -cookie          use cookie (-no-cookie: don't use cookie)\n");
+    PUT("-cookie", "use cookie (-no-cookie: don't use cookie)");
 #endif				/* USE_COOKIE */
-    fprintf(f, "    -graph           use DEC special graphics for border of table and menu\n");
-    fprintf(f, "    -no-graph        use ASCII character for border of table and menu\n");
-    fprintf(f, "    -s               squeeze multiple blank lines\n");
-    fprintf(f, "    -W               toggle search wrap mode\n");
-    fprintf(f, "    -X               don't use termcap init/deinit\n");
-    fprintf(f,
-	    "    -title[=TERM]    set buffer name to terminal title string\n");
-    fprintf(f, "    -o opt=value     assign value to config option\n");
-    fprintf(f, "    -show-option     print all config options\n");
-    fprintf(f, "    -config file     specify config file\n");
-    fprintf(f, "    -debug           use debug mode (only for debugging)\n");
-    fprintf(f, "    -reqlog          write request logfile\n");
-    fprintf(f, "    -help            print this usage message\n");
-    fprintf(f, "    -version         print w3m version\n");
+    PUT("-graph", "use DEC special graphics for border of table and menu");
+    PUT("-no-graph", "use ASCII character for border of table and menu");
+    PUT("-s", "squeeze multiple blank lines");
+    PUT("-W", "toggle search wrap mode");
+    PUT("-X", "don't use termcap init/deinit");
+    PUT("-title[=TERM]", "set buffer name to terminal title string");
+    PUT("-o opt=value", "assign value to config option");
+    PUT("-show-option", "print all config options");
+    PUT("-config file", "specify config file");
+    PUT("-debug", "use debug mode (only for debugging)");
+    PUT("-reqlog", "write request logfile");
+    PUT("-help", "print this usage message");
+    PUT("-version", "print w3m version");
     if (show_params_p)
 	show_params(f);
     exit(err);
 }
+#undef PUT
 
 #ifdef USE_M17N
 #ifdef __EMX__
